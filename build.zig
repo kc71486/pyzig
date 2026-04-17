e16b2d602d38bc8ce1e20351d055190fc445439c
kc71486 <linda20030102@gmail.com>
2026-04-17 18:10:45 +0800

    Use a better default for sys_include.
    Partial revert of commit ffa5861.

--- build.zig
+++ build.zig
@@ -15,10 +15,11 @@ pub fn build(b: *std.Build) !void {
 
     // option refine
     const sys_include: []const u8 = o_sys_include orelse
-        if (target.result.os.tag == .windows)
-            "C:\\Users\\user\\Appdata\\Local\\Programs\\Python\\Python312\\include"
-        else
-            "/usr/include/python3.12/";
+        if (target.result.os.tag == .windows) blk: {
+            const home: []const u8 = try getEnvVarOwnedW(b.allocator, "USERPROFILE");
+            defer b.allocator.free(home);
+            break :blk try std.fs.path.join(b.allocator, &.{ home, "Appdata\\Local\\Programs\\Python\\Python312\\include" });
+        } else "/usr/include/python3.12/";
 
     // python ffi cimport module, requires release build.
     // debug build includes symbol that doesn't exist in python312.lib.
@@ -58,5 +59,90 @@ pub fn build(b: *std.Build) !void {
     step_test.dependOn(step_install);
 }
 
+/// Windows-only. Caller must free returned memory.
+/// If `key` is not valid [WTF-8](https://simonsapin.github.io/wtf-8/),
+/// then `error.InvalidWtf8` is returned.
+/// The value is encoded as [WTF-8](https://simonsapin.github.io/wtf-8/).
+fn getEnvVarOwnedW(allocator: Allocator, key: []const u8) GetEnvVarOwnedError![]u8 {
+    const result_w = blk: {
+        var fba_buf: [256 * @sizeOf(u16)]u8 = undefined;
+        var fba: std.heap.FixedBufferAllocator = .init(&fba_buf);
+        const buffer_allocator: Allocator = fba.allocator();
+        const key_w: [:0]u16 = try unicode.wtf8ToWtf16LeAllocZ(buffer_allocator, key);
+        defer buffer_allocator.free(key_w);
+
+        break :blk getenvW(key_w) orelse return GetEnvVarOwnedError.EnvironmentVariableNotFound;
+    };
+    // wtf16LeToWtf8Alloc can only fail with OutOfMemory
+    return unicode.wtf16LeToWtf8Alloc(allocator, result_w);
+}
+
+/// Windows-only. Get an environment variable with a null-terminated, WTF-16 encoded name.
+///
+/// This function performs a Unicode-aware case-insensitive lookup using RtlEqualUnicodeString.
+fn getenvW(key: [*:0]const u16) ?[:0]const u16 {
+    const key_slice = std.mem.sliceTo(key, 0);
+    // '=' anywhere but the start makes this an invalid environment variable name
+    if (key_slice.len > 0 and std.mem.indexOfScalar(u16, key_slice[1..], '=') != null) {
+        return null;
+    }
+    const ptr = windows.peb().ProcessParameters.Environment;
+    var i: usize = 0;
+    while (ptr[i] != 0) {
+        const key_value = std.mem.sliceTo(ptr[i..], 0);
+
+        // There are some special environment variables that start with =,
+        // so we need a special case to not treat = as a key/value separator
+        // if it's the first character.
+        // https://devblogs.microsoft.com/oldnewthing/20100506-00/?p=14133
+        const equal_search_start: usize = if (key_value[0] == '=') 1 else 0;
+        const equal_index = std.mem.indexOfScalarPos(u16, key_value, equal_search_start, '=') orelse {
+            // This is enforced by CreateProcess.
+            // If violated, CreateProcess will fail with INVALID_PARAMETER.
+            unreachable; // must contain a =
+        };
+
+        const this_key = key_value[0..equal_index];
+        if (eqlIgnoreCaseWTF16(key_slice, this_key)) {
+            return key_value[equal_index + 1 ..];
+        }
+
+        // skip past the NUL terminator
+        i += key_value.len + 1;
+    }
+    return null;
+}
+
+/// Compares two WTF16 strings using the equivalent functionality of
+/// `RtlEqualUnicodeString` (with case insensitive comparison enabled).
+fn eqlIgnoreCaseWTF16(a: []const u16, b: []const u16) bool {
+    if (a.len != b.len) return false;
+
+    for (a, b) |a_c, b_c| {
+        // The slices are always WTF-16 LE, so need to convert the elements to native
+        // endianness for the uppercasing
+        const a_c_native = std.mem.littleToNative(u16, a_c);
+        const b_c_native = std.mem.littleToNative(u16, b_c);
+        if (a_c != b_c and windows.nls.upcaseW(a_c_native) != windows.nls.upcaseW(b_c_native)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+pub const GetEnvVarOwnedError = error{
+    OutOfMemory,
+    EnvironmentVariableNotFound,
+    /// On Windows, environment variable keys provided by the user must be valid WTF-8.
+    /// https://simonsapin.github.io/wtf-8/
+    InvalidWtf8,
+};
+
+const builtin = @import("builtin");
+const native_os = builtin.os.tag;
+
 const std = @import("std");
 const Step = std.Build.Step;
+const unicode = std.unicode;
+const windows = std.os.windows;
+const Allocator = std.mem.Allocator;
